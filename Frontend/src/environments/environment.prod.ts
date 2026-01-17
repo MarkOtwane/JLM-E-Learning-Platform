cf427b9eced6c446ef954e26a30d74dd8cf8e812
MarkOtwane <otwanemark254@gmail.com>
2026-01-17 13:54:32 +0300

    fix: update the apiUrl on enviroment file on frontend

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api', // Update this with your actual production API URL
+  apiUrl: 'https://jlm-e-learning-platform.onrender.com', // Update this with your actual production API URL
   // Add other environment-specific variables here
   appName: 'JLM E-Learning Platform',
   version: '1.0.0',
